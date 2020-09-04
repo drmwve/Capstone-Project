fe4711a568bae6b1b56a3d43fa9e67aa8e11209f
drmwve <62453769+drmwve@users.noreply.github.com>
2020-09-03 19:29:35 -0700

    first functional pass on pickler code

--- src/ExecutionCode/BrewRecipe.py
+++ src/ExecutionCode/BrewRecipe.py
@@ -1,9 +1,58 @@
+try:
+    import cPickle as pickle
+except:
+    import pickle
+import hashlib
+import hmac
+from dataclasses import dataclass, field
+from loguru import logger
+
+@dataclass
 class BrewRecipe():
     # I'm a messenger class which holds information about the brew recipe such as
     # hop timing, mash temperature, etc. I am created by the BrewConfig UI screen
     # and passed to a Brew Process in the signal that starts the brewing process.
-    def __init__(self):
-        self.HopCartridges = 5
-        self.MashTunTemperature = 160
-        self.hopTiming = [0,0,0,0,0]
-    pass
+    name: str = "Default"
+    hopCartridges: int = 5
+    mashTunTemperature: int = 160
+    hopTiming: int = field(default_factory=lambda: [0,0,0,0,0])
+
+class BrewRecipePickler(object):
+    _instance = None
+    _picklefile = "brewrecipes.pkl"
+
+    def __new__(cls):
+        if cls._instance is None:
+            print('Creating the object')
+            cls._instance = super(BrewRecipePickler, cls).__new__(cls)
+            # Put any initialization here.
+        return cls._instance
+
+
+    def saveRecipes(self, brewRecipes):
+        recipePickle = pickle.dumps(brewRecipes)
+        pickleDigest = self.make_digest(recipePickle)
+        with open(BrewRecipePickler._picklefile, "w") as picklefile:
+            picklefile.write(str(pickleDigest) + '\n')
+            picklefile.write(str(recipePickle))
+
+    def loadRecipes(self):
+        logger.debug("Attempting to load recipes")
+        try:
+            with open(BrewRecipePickler._picklefile, "r") as picklefile:
+                pickleDigest, recipePickle = picklefile.readlines(2)
+                checkPD = self.make_digest(pickleDigest)
+                if hmac.compare_digest(checkPD, pickleDigest):
+                    brewRecipes = pickle.loads(recipePickle)
+                logger.debug("Loading " + brewRecipes)
+        except:
+            brewRecipes = [BrewRecipe()]
+            self.saveRecipes(brewRecipes)
+            logger.debug("Creating new brew recipes file")
+        return brewRecipes
+
+    def make_digest(self, message):
+        hash = hmac.new(bytes(192),
+                    message,
+                    hashlib.sha1)
+        return hash.hexdigest()
--- src/GUI/BrewConfig.py
+++ src/GUI/BrewConfig.py
@@ -1,16 +1,17 @@
 from PySide2 import QtCore, QtGui, QtWidgets
 from loguru import logger
-from ExecutionCode.BrewRecipe import BrewRecipe
+from ExecutionCode.BrewRecipe import BrewRecipe, BrewRecipePickler
 from GUI.BrewConfigGUI import Ui_BrewConfigWindow
 from functools import partial
 import linecache
-import pickle
 
 class BrewConfig(QtWidgets.QWidget,Ui_BrewConfigWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-
+        self.pickler = BrewRecipePickler()
+        self.savedBrewRecipes = self.pickler.loadRecipes()
+        self.selectedBrewRecipe = self.savedBrewRecipes[0]
 
         self.hopEntry = [self.Hop1Entry, self.Hop2Entry, self.Hop3Entry, self.Hop4Entry, self.Hop5Entry]
         self.hopIncrease = [self.Hop1Increase, self.Hop2Increase, self.Hop3Increase, self.Hop4Increase, self.Hop5Increase]
@@ -21,14 +22,14 @@ class BrewConfig(QtWidgets.QWidget,Ui_BrewConfigWindow):
     def connections(self):
         self.MashTempDecrease.clicked.connect(self.DecreaseMashTemp)
         self.MashTempIncrease.clicked.connect(self.IncreaseMashTemp)
-        self.MashTempEntry.setText(str(self.MashTunTemperature))
+        self.MashTempEntry.setText(str(self.selectedBrewRecipe.mashTunTemperature))
 
-        self.HopCartridgeSelectEntry.setText(str(self.HopCartridges))
+        self.HopCartridgeSelectEntry.setText(str(self.selectedBrewRecipe.hopCartridges))
         self.HopCartridgeSelectDecrease.clicked.connect(self.DecreaseCartridgeSelect)
         self.HopCartridgeSelectIncrease.clicked.connect(self.IncreaseCartridgeSelect)
 
         for index, hopRow in enumerate(self.hopEntry):
-            self.hopEntry[index].setText(str(self.hopTiming[index]))
+            self.hopEntry[index].setText(str(self.selectedBrewRecipe.hopTiming[index]))
             self.hopIncrease[index].clicked.connect(partial(self.increaseHop, index))
             self.hopDecrease[index].clicked.connect(partial(self.decreaseHop, index))
 
@@ -45,46 +46,46 @@ class BrewConfig(QtWidgets.QWidget,Ui_BrewConfigWindow):
 
     ## Defining button functions
     def IncreaseMashTemp(self):
-        self.MashTunTemperature += 1
-        self.MashTempEntry.setText(str(self.MashTunTemperature))
+        self.selectedBrewRecipe.mashTunTemperature += 1
+        self.MashTempEntry.setText(str(self.selectedBrewRecipe.mashTunTemperature))
 
     def DecreaseMashTemp(self):
-        self.MashTunTemperature -= 1
-        self.MashTempEntry.setText(str(self.MashTunTemperature))
+        self.selectedBrewRecipe.mashTunTemperature -= 1
+        self.MashTempEntry.setText(str(self.selectedBrewRecipe.mashTunTemperature))
 
     def IncreaseCartridgeSelect(self):
-        if self.HopCartridges < 5:
-            self.HopCartridges += 1
-            self.HopCartridgeSelectEntry.setText(str(self.HopCartridges))
-            self.hopEntry[self.HopCartridges - 1].setHidden(False)
-            self.hopIncrease[self.HopCartridges - 1].setHidden(False)
-            self.hopDecrease[self.HopCartridges - 1].setHidden(False)
-            self.hopTiming[self.HopCartridges - 1] = 0
-            self.hopEntry[self.HopCartridges - 1].setText("0")
+        if self.selectedBrewRecipe.hopCartridges < 5:
+            self.selectedBrewRecipe.hopCartridges += 1
+            self.HopCartridgeSelectEntry.setText(str(self.selectedBrewRecipe.hopCartridges))
+            self.hopEntry[self.selectedBrewRecipe.hopCartridges - 1].setHidden(False)
+            self.hopIncrease[self.selectedBrewRecipe.hopCartridges - 1].setHidden(False)
+            self.hopDecrease[self.selectedBrewRecipe.hopCartridges - 1].setHidden(False)
+            self.selectedBrewRecipe.hopTiming[self.selectedBrewRecipe.hopCartridges - 1] = 0
+            self.hopEntry[self.selectedBrewRecipe.hopCartridges - 1].setText("0")
 
     def DecreaseCartridgeSelect(self):
-        if self.HopCartridges > 1:
-            self.HopCartridges -= 1
-            self.HopCartridgeSelectEntry.setText(str(self.HopCartridges))
-            self.hopEntry[self.HopCartridges].setHidden(True)
-            self.hopIncrease[self.HopCartridges].setHidden(True)
-            self.hopDecrease[self.HopCartridges].setHidden(True)
-            self.hopTiming[self.HopCartridges] = -1
+        if self.selectedBrewRecipe.hopCartridges > 1:
+            self.selectedBrewRecipe.hopCartridges -= 1
+            self.HopCartridgeSelectEntry.setText(str(self.selectedBrewRecipe.hopCartridges))
+            self.hopEntry[self.selectedBrewRecipe.hopCartridges].setHidden(True)
+            self.hopIncrease[self.selectedBrewRecipe.hopCartridges].setHidden(True)
+            self.hopDecrease[self.selectedBrewRecipe.hopCartridges].setHidden(True)
+            self.selectedBrewRecipe.hopTiming[self.selectedBrewRecipe.hopCartridges] = -1
 
     def increaseHop(self, index):
-        if self.hopTiming[index] < 60:
-            self.hopTiming[index] +=5
-            self.hopEntry[index].setText(str(self.hopTiming[index]))
+        if self.selectedBrewRecipe.hopTiming[index] < 60:
+            self.selectedBrewRecipe.hopTiming[index] +=5
+            self.hopEntry[index].setText(str(self.selectedBrewRecipe.hopTiming[index]))
 
     def decreaseHop(self, index):
-        if self.hopTiming[index] > 0:
-            self.hopTiming[index] -=5
-            self.hopEntry[index].setText(str(self.hopTiming[index]))
+        if self.selectedBrewRecipe.hopTiming[index] > 0:
+            self.selectedBrewRecipe.hopTiming[index] -=5
+            self.hopEntry[index].setText(str(self.selectedBrewRecipe.hopTiming[index]))
 
     def StartBrewing(self):
         ## This function should connect to Husam's brewing program
         print("I need connected to the brewing program")
-        logger.info("Starting brew cycle with parameters: ""MTT:"+str(self.MashTunTemperature)+" HC:"+str(self.HopCartridges)+" HT:"+str(self.hopTiming))
+        logger.info("Starting brew cycle with parameters: " + self.selectedBrewRecipe)
 
 
     def toggleLoad(self):
@@ -98,27 +99,23 @@ class BrewConfig(QtWidgets.QWidget,Ui_BrewConfigWindow):
     def quickBrew(self, index):
         ## Load a brew
         if self.QBLoadButton.isChecked():
-            logger.info("Read QuickBrew file "+str(index+1))
-            
-            self.outfile = open('filename.txt','rb')
-            self.quickBrewP = pickle.load(self.outfile)
-            print(self.quickBrewP)
-            
-            ## Print retrieved settings
-            logger.info("Brew parameters set to: ""MTT:"+str(self.MashTunTemperature)+" HC:"+str(self.HopCartridges)+" HT:"+str(self.hopTiming))
+            self.selectedBrewRecipe = self.savedBrewRecipes[index]
+            logger.info("Read QuickBrew file "+str(index+1)+ ": " + self.selectedBrewRecipe)
+
+
             ## Reset text field displays
             for i in range(0,5):
-                self.hopEntry[i].setText(str(self.hopTiming[i]))
-            self.HopCartridgeSelectEntry.setText(str(self.HopCartridges))
-            self.MashTempEntry.setText(str(self.MashTunTemperature))
+                self.hopEntry[i].setText(str(self.selectedBrewRecipe.hopTiming[i]))
+            self.HopCartridgeSelectEntry.setText(str(self.selectedBrewRecipe.hopCartridges))
+            self.MashTempEntry.setText(str(self.selectedBrewRecipe.mashTunTemperature))
             ## Show all hop fields
             for i in range(0,5):
                 self.hopEntry[i].setHidden(False)
                 self.hopIncrease[i].setHidden(False)
                 self.hopDecrease[i].setHidden(False)
             ## Hide irrelevant hop fields
-            if self.HopCartridges < 5:
-                for i in range(self.HopCartridges, 5):
+            if self.selectedBrewRecipe.hopCartridges < 5:
+                for i in range(self.selectedBrewRecipe.hopCartridges, 5):
                     self.hopEntry[i].setHidden(True)
                     self.hopIncrease[i].setHidden(True)
                     self.hopDecrease[i].setHidden(True)
@@ -128,13 +125,9 @@ class BrewConfig(QtWidgets.QWidget,Ui_BrewConfigWindow):
             #self.qbFile = open('src\GUI\QuickBrewSaves\QuickBrew%d.txt'%(index,), 'w')
             ## Logging
             #logger.info("Saved QuickBrew file "+str(index+1))
-            #logger.info("Brew parameters saved: ""MTT:"+str(self.MashTunTemperature)+" HC:"+str(self.HopCartridges)+" HT:"+str(self.hopTiming))
+            #logger.info("Brew parameters saved: ""MTT:"+str(self.selectedBrewRecipe.mashTunTemperature)+" HC:"+str(self.HopCartridges)+" HT:"+str(self.selectedBrewRecipe.hopTiming))
 
-            self.quickBrewP = [self.MashTunTemperature, self.HopCartridges, self.hopTiming]
-            print(self.quickBrewP)
-            self.outfile = open('filename.txt','wb')
-            self.quickBrewPickle = pickle.dump(self.quickBrewP,self.outfile)
-            self.outfile.close()
+            self.pickler.saveRecipes(self.savedBrewRecipes)
 
             ## Close the file
             #self.qbFile.close()
