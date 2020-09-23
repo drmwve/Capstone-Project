0f1cd0cd87246789d7b37fb738f243151439f7e6
drmwve <62453769+drmwve@users.noreply.github.com>
2020-09-23 15:08:02 -0700

    refactor steps

--- autobrewer/steps.py
+++ autobrewer/steps.py
@@ -1,7 +1,16 @@
 from .hardware.devicehandler import DeviceHandler
+from PySide2.QtCore import Signal, QObject
+
+class Step(QObject):
+    stepcomplete = Signal()
+    stepstarted = Signal(str)
+
+    def __init__(self):
+        super().__init__()
 
 class ExampleStep():
     def __call__(self):
+        self.stepstarted.emit()
         DeviceHandler.open2WBallValve(3)
 
 class fillHTL():
